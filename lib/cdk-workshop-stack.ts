060f02c2fcb67e4608ce92d6fb6ef57f56239d11
danimal141 <hideaki.ishii1204@gmail.com>
2019-10-22 16:39:10 +0900

    Add API gateway endpoint

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,15 +1,19 @@
 import cdk = require('@aws-cdk/core')
 import lambda = require('@aws-cdk/aws-lambda')
+import apigateway = require('@aws-cdk/aws-apigateway')
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // defines an AWS Lambda resource
-    new lambda.Function(this, 'HelloHandler', {
+    // Define an AWS Lambda resource
+    const helloFunc = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_8_10, // execution environment
-      code: lambda.Code.asset('lambda'), // code loaded from the "lambda" directory
-      handler: 'hello.handler' // file is "hello", function is "handler"
+      code: lambda.Code.asset('lambda'), // code loaded from the 'lambda' directory
+      handler: 'hello.handler' // file is 'hello', function is 'handler'
     })
+
+    // Define an API Gateway REST API resource backed by the above function
+    new apigateway.LambdaRestApi(this, 'Endpoint', { handler: helloFunc })
   }
 }
